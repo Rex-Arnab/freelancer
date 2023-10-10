8d4801dfcea1d967624f26e56facd9bc08aca193
Arnab Biswas <khanp4397@gmail.com>
2023-10-10 19:58:22 +0530

    fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Card = () => {
   return (
-    <div className="w-full bg-slate-200 p-5 rounded flex items-center">
+    <div className="w-full bg-slate-200 p-5 rounded grid grid-cols-1 md:flex md:items-center">
       <div className="relative w-10 h-10 rounded-xl">
         <Image
           src="https://www.f-cdn.com/assets/main/en/assets/default-notification-image.svg"
@@ -10,7 +10,7 @@ const Card = () => {
           fill
         />
       </div>
-      <div className="px-5">
+      <div className="p-0 md:px-5">
         <p>
           Welcome User, Get Noticed today by taking our US English Exam - Level
           1
