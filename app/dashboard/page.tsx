ea16890b72acaabca606b2da4084fac3ebcacc81
Arnab Biswas <khanp4397@gmail.com>
2023-12-05 22:55:12 +0530

    removed default card

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,26 +4,26 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import moment from "moment";
 
-const DefaultCard = () => {
-  return (
-    <div className="w-full bg-slate-200 p-5 rounded grid grid-cols-1 md:flex md:items-center">
-      <div className="relative w-10 h-10 rounded-xl">
-        <Image
-          src="https://www.f-cdn.com/assets/main/en/assets/default-notification-image.svg"
-          alt="Zero Commission"
-          fill
-        />
-      </div>
-      <div className="p-0 md:px-5">
-        <p>
-          Welcome User, Get Noticed today by taking our US English Exam - Level
-          1
-        </p>
-        <span>5 minutes ago</span>
-      </div>
-    </div>
-  );
-};
+// const DefaultCard = () => {
+//   return (
+//     <div className="w-full bg-slate-200 p-5 rounded grid grid-cols-1 md:flex md:items-center">
+//       <div className="relative w-10 h-10 rounded-xl">
+//         <Image
+//           src="https://www.f-cdn.com/assets/main/en/assets/default-notification-image.svg"
+//           alt="Zero Commission"
+//           fill
+//         />
+//       </div>
+//       <div className="p-0 md:px-5">
+//         <p>
+//           Welcome User, Get Noticed today by taking our US English Exam - Level
+//           1
+//         </p>
+//         <span>5 minutes ago</span>
+//       </div>
+//     </div>
+//   );
+// };
 
 interface CardProps {
   todo: any;
@@ -56,11 +56,8 @@ function Dashboard() {
   }, []);
   return (
     <div>
-      {todoList.length > 0 ? (
-        todoList.map((todo: any) => <Card key={todo._id} todo={todo} />)
-      ) : (
-        <DefaultCard />
-      )}
+      {todoList.length > 0 &&
+        todoList.map((todo: any) => <Card key={todo._id} todo={todo} />)}
     </div>
   );
 }
